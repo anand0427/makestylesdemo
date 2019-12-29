974cfae2e7ae0582770560e0ce5194010bbf59dd
readysetcodein <readysetcodein@gmail.com>
2019-12-29 22:43:58 +0530

    First Commit. Demo of makestyle in material design. Paper and Grid.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const useStyles = makeStyles(theme => ({
 
+    root:{
+      flexGrow:1,
+    },
+    paper:{
+      padding : theme.spacing(2),
+      textAlign : 'center',
+      color : theme.palette.text.secondary,
+    }
+
+  }));
+  const theme = createMuiTheme({
+  palette : {
+    background : {
+      default : "#f5f5f5"
+    }
+  }
+});
+
+    const classes = useStyles();
+    return (
+      <MuiThemeProvider theme={theme}> <CssBaseline />
+      <div className={classes.root}>
+
+        <Grid container spacing = {3}>
+              <Grid item xs = {3}>
+              <Paper className = {classes.paper} > Paper with padding space 2 and
+              color secondary </Paper>
+              </Grid>
+              <Grid item xs = {3}>
+              <Paper className = {classes.paper} > Paper gives material design.
+              makestyles method
+              is called along with the theme which intern calls
+              stylequeryselector which checks if
+              we send a function which is why theme is a function </Paper>
+              </Grid>
+          </Grid>
+
+      </div>
+      </MuiThemeProvider>
+    );
+}
 export default App;
